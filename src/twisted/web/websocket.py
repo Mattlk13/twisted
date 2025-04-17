dace2874df767d2134d6e2a0865a93e5c2a9ad0b
Glyph <glyph@twistedmatrix.com>
2025-04-17 11:06:49 -0700

    Update src/twisted/web/websocket.py
    Co-authored-by: Adi Roiban <adiroiban@gmail.com>

--- src/twisted/web/websocket.py
+++ src/twisted/web/websocket.py
@@ -5,7 +5,8 @@ Websocket (rfc6455) client and server support.
 For websocket servers, place a L{WebSocketResource} into your Twisted Web
 resource hierarchy.
 
-For websocket clients, call L{WebSocketClientEndpoint.connect}.
+For websocket clients, create a new endpoint via L{WebSocketClientEndpoint.new} with the WebSocket server URL
+and then, on the newly created endpoint, call L{WebSocketClientEndpoint.connect}.
 
 @note: To use this module, you must install Twisted's C{websocket} extra, i.e.
     C{pip install twisted[websocket]}.
